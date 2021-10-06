1c5a3d8798244a20f773a780e511e70784980630
NanezX <victor.nanezj@gmail.com>
2021-10-06 02:03:43 -0400

    Reef networks

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -6,6 +6,7 @@ import "@nomiclabs/hardhat-waffle";
 import "@typechain/hardhat";
 import "hardhat-gas-reporter";
 import "solidity-coverage";
+import "@reef-defi/hardhat-reef";
 
 dotenv.config();
 
@@ -22,13 +23,25 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
-const config: HardhatUserConfig = {
-  solidity: "0.8.4",
+const config = {
+  solidity: "0.6.12",
   networks: {
     ropsten: {
       url: process.env.ROPSTEN_URL || "",
       accounts:
         process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    reef_mainnet: {
+      url: "wss://rpc.reefscan.com/ws",
+      seeds: {
+        "account": process.env.MNEMONIC
+      }
+    },
+    reef_testnet: {
+      url: "wss://rpc-testnet.reefscan.com/ws",
+      seeds: {
+        "account": process.env.MNEMONIC,
+      }
     },
   },
   gasReporter: {
