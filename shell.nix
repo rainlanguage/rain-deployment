3f42c5296e15d02a11f57075f5f2f89579d91028
NanezX <victor.nanezj@gmail.com>
2022-03-14 16:05:45 -0400

    new nix

--- shell.nix
+++ shell.nix
@@ -1,8 +1,14 @@
 let
- pkgs = import <nixpkgs> {};
+  pkgs = import
+    (builtins.fetchTarball {
+      name = "nixos-unstable-2021-10-01";
+      url = "https://github.com/nixos/nixpkgs/archive/d3d2c44a26b693293e8c79da0c3e3227fc212882.tar.gz";
+      sha256 = "0vi4r7sxzfdaxzlhpmdkvkn3fjg533fcwsy3yrcj5fiyqip2p3kl";
+    })
+    { };
 
    flush-all = pkgs.writeShellScriptBin "flush-all" ''
-    # rm -rf node_modules
+    rm -rf node_modules
     rm -rf artifacts
     rm -rf cache
     rm -rf typechain
@@ -10,34 +16,13 @@ let
     yarn install
   '';
 
-   deploy-rain = pkgs.writeShellScriptBin "deploy-rain" ''
+  deploy-rain = pkgs.writeShellScriptBin "deploy-rain" ''
     npx hardhat run scripts/deploy-rain.ts --network ''$1
   '';
 
-   create-trust = pkgs.writeShellScriptBin "create-trust" ''
+  create-trust = pkgs.writeShellScriptBin "create-trust" ''
     export TrustFactory=''$1
     npx hardhat run scripts/create-trust.ts --network ''$2
-  '';
-
-   deploy-verify = pkgs.writeShellScriptBin "deploy-verify" ''
-    export AdminAddress=''$1
-    npx hardhat run scripts/deploy-verify-tier.ts --network ''$2
-  '';
-
-  cut-dist = pkgs.writeShellScriptBin "cut-dist" ''
-    // WIP
-    if [ -z "$1" ]; then echo "ERROR: Missing commit argument. Exiting..."; exit 0; fi
-    flush-all
-    commit=$1; copy-commit $1
-    sed -i '$s/.*/COMMIT='$commit'/' .env
-    hardhat compile --force
-    rm -rf dist
-    mkdir -p "dist"
-    solt-the-earth
-
-    cp -r "artifacts" "dist"
-    cp -r "typechain" "dist"
-    cp -r "solt" "dist"
   '';
 
   solt-the-earth = pkgs.writeShellScriptBin "solt-the-earth" ''
@@ -66,6 +51,14 @@ let
     cp -r "node_modules/@beehiveinnovation/rain-statusfi/solt/solc-input-gatednftfactory.json" "solt"
     cp -r "node_modules/@beehiveinnovation/rain-statusfi/solt/solc-input-gatednft.json" "solt"
   '';
+
+  init = pkgs.writeShellScriptBin "init" ''
+    mkdir -p contracts && cp -r node_modules/@beehiveinnovation/rain-protocol/contracts .
+    mkdir -p contracts/rain-statusfi && cp node_modules/@beehiveinnovation/rain-statusfi/contracts/*.sol contracts/rain-statusfi
+    mkdir -p contracts/tier && cp node_modules/@vishalkale15107/rain-protocol/contracts/tier/ERC721BalanceTier*.sol contracts/tier
+    mkdir -p contracts/test && cp node_modules/@vishalkale15107/rain-protocol/contracts/test/ReserveNFT.sol contracts/test
+    npx hardhat compile
+  '';
   
 in
 pkgs.stdenv.mkDerivation {
@@ -74,13 +67,13 @@ pkgs.stdenv.mkDerivation {
   pkgs.yarn
   pkgs.nodejs-16_x
   pkgs.jq
+  flush-all
   deploy-rain
   create-trust
   deploy-verify
-  flush-all
-  cut-dist
   solt-the-earth
   get-commit
+  init
  ];
 
  shellHook = ''
@@ -88,9 +81,6 @@ pkgs.stdenv.mkDerivation {
   export PATH=$( npm bin ):$PATH
   # keep it fresh
   yarn install
-  (cd node_modules/@vishalkale15107/rain-protocol && [ ! -d artifacts ] && npm install --ignore-scripts && npm run build)
-  (cd node_modules/@beehiveinnovation/rain-statusfi && [ ! -d artifacts ] && npm install --ignore-scripts && npm run build)
-  get-commit
  '';
 }
 
