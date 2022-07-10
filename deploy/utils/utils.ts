d28ff3530022fd77723f0a66282ae218e290bc06
NanezX <victor.nanezj@gmail.com>
2022-07-09 21:19:19 -0400

    New chains deployed - Arbitrum (Mainnet) - Aurora (Mainnet and testnet) - Avalanche (Mainnet) - Celo (Mainnet and testnet) - Fantom (Mainnet and testnet)

--- deploy/utils/utils.ts
+++ deploy/utils/utils.ts
@@ -4,22 +4,29 @@ import fs, { promises as fsPromises } from "fs";
 import fse from "fs-extra";
 import hre, { artifacts, ethers } from "hardhat";
 
+// Types
 import type { BigNumber, BytesLike, Overrides } from "ethers";
-import { utils } from "ethers";
-
 import type { Artifact, CompilerOutputContract } from "hardhat/types";
-import { FeeData } from "@ethersproject/abstract-provider";
-
-import deployConfig from "../../deployment-config.json";
-
-import {
+import type { FeeData } from "@ethersproject/abstract-provider";
+import type { PayableOverrides, Contract } from "@ethersproject/contracts";
+import type {
   StateConfigStruct,
   NewChildEvent,
 } from "../../typechain/CombineTierFactory";
+
+// Ethers
+import { ContractFactory } from "@ethersproject/contracts";
+import { utils } from "ethers";
+
+// CombineTier factory
 import { CombineTierFactory__factory } from "../../typechain/factories/CombineTierFactory__factory";
 import { CombineTier__factory } from "../../typechain/factories/CombineTier__factory";
 
-export const networkName = hre.network.name;
+// Deployment configuration
+import deployConfig from "../../deployment-config.json";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+
+const networkName = hre.network.name;
 const commit: string = process.env.COMMIT;
 const ZERO_BN = ethers.constants.Zero;
 
@@ -27,7 +34,8 @@ const deploymentsPath = path.resolve(__dirname, "../../deployments");
 if (!fs.existsSync(`${deploymentsPath}/${commit}`)) {
   fs.mkdirSync(`${deploymentsPath}/${commit}`, { recursive: true });
 }
-export enum AllStandardOps {
+
+enum AllStandardOps {
   SKIP,
   VAL,
   DUP,
@@ -77,14 +85,14 @@ export enum AllStandardOps {
   length,
 }
 
-export const CombineTierOpcodes = {
+const CombineTierOpcodes = {
   ...AllStandardOps,
   ACCOUNT: 0 + AllStandardOps.length,
 };
 
-export const sourceAlways = utils.concat([op(CombineTierOpcodes.ALWAYS)]);
+const sourceAlways = utils.concat([op(CombineTierOpcodes.ALWAYS)]);
 
-export interface BasicArtifact extends Partial<Artifact> {
+interface BasicArtifact extends Partial<Artifact> {
   contractName: string;
   abi: any[];
   bytecode: string;
@@ -93,13 +101,13 @@ export interface BasicArtifact extends Partial<Artifact> {
   userdoc?: any;
 }
 
-export interface OutputContract extends CompilerOutputContract {
+interface OutputContract extends CompilerOutputContract {
   metadata?: string;
   userdoc?: any;
   devdoc?: any;
 }
 
-export interface DeployOptions {
+interface DeployOptions {
   contract?: BasicArtifact;
   args: any[];
   from: string;
@@ -114,7 +122,7 @@ export interface DeployOptions {
   waitConfirmations?: number;
 }
 
-export interface DeployResult {
+interface DeployResult {
   address: string;
   abi: any[];
   transactionHash?: string;
@@ -129,7 +137,7 @@ export interface DeployResult {
   devdoc?: any;
 }
 
-export type Receipt = {
+type Receipt = {
   from: string;
   transactionHash: string;
   blockHash: string;
@@ -147,7 +155,7 @@ export type Receipt = {
   confirmations?: number;
 };
 
-export type Log = {
+type Log = {
   blockNumber: number;
   blockHash: string;
   transactionHash: string;
@@ -163,7 +171,54 @@ export enum EstimationLevel {
   LOW, // Will use a -10% of current market value
   MARKET, // Will use the current market value
   AGGRESSIVE, // Will use a +10% of current market value
+  FIXED, // Will use values provided
 }
+
+/**
+ * These networks does not support certains functions than require few ask to the provider and they will
+ * fail with hardhat-deploy plugin, so they will be handled separately and the gasPrice should be fixed;
+ */
+const specialNetworks = [
+  "celo_mainnet",
+  "celo_alfajores",
+  "aurora_mainnet",
+  "aurora_testnet",
+];
+
+/**
+ * Estimate the gas that will be use to deploy the contract.
+ * The contract provided could be a artifact or a contract
+ * name that will be searched with Hardhat.
+ * @param contract The contract that will be deployed.
+ * @param args The arguments necessaries to deploy
+ * @returns The gas estimation
+ */
+export const estimateGasDeploy = async (
+  contract: string | Artifact | BasicArtifact,
+  args: any[] = []
+): Promise<BigNumber> => {
+  let artifact: Artifact | BasicArtifact;
+
+  if (typeof contract === "string") {
+    artifact = await artifacts.readArtifact(contract);
+  } else {
+    artifact = contract;
+  }
+
+  const iface = new ethers.utils.Interface(artifact.abi);
+  const factory = new ethers.ContractFactory(iface, artifact.bytecode);
+
+  const numArguments = factory.interface.deploy.inputs.length;
+  if (args.length !== numArguments) {
+    throw new Error(
+      `expected ${numArguments} constructor arguments, got ${args.length}`
+    );
+  }
+
+  const data = factory.getDeployTransaction(...args).data;
+
+  return await ethers.provider.estimateGas({ data: data });
+};
 
 export async function deployContract(
   contractName: string,
@@ -182,6 +237,9 @@ export async function deployContract(
   if (estimation === "aggressive") {
     estimationLevel = EstimationLevel.AGGRESSIVE;
   }
+  if (estimation === "fixed") {
+    estimationLevel = EstimationLevel.FIXED;
+  }
 
   const feeCalculated = await estimateGasFee(estimationLevel);
   if (!options.gasPrice) {
@@ -194,8 +252,54 @@ export async function deployContract(
     options.maxPriorityFeePerGas = feeCalculated.maxPriorityFeePerGas;
   }
 
-  const result = await deploy(contractName, options);
-  return result;
+  if (!specialNetworks.includes(networkName)) {
+    return await deploy(contractName, options);
+  } else {
+    // see deployOptional();
+    return await deployOptional(contractName, options);
+  }
+}
+
+/**
+ * Deploy a contract without making any request to the provider about info
+ *
+ * @remarks
+ * This is intended to be used when deploy-hardhat is not available or when the provider cannot
+ * return any information
+ *
+ * @param name Name of contract to be deployed
+ * @param options Deployment options and configuration
+ * @returns The deployment result
+ */
+export async function deployOptional(
+  name: string,
+  options: DeployOptions
+): Promise<DeployResult> {
+  const signers = await ethers.getSigners();
+  const signer = signers.find(
+    (x) => x.address.toLowerCase() === options.from.toLowerCase()
+  );
+
+  const overrides: PayableOverrides = {
+    gasLimit: options.gasLimit,
+    gasPrice: options.gasPrice,
+    maxFeePerGas: options.maxFeePerGas,
+    maxPriorityFeePerGas: options.maxPriorityFeePerGas,
+    value: options.value,
+    nonce: options.nonce,
+  };
+
+  const { artifact, factory, args } = await getContract(name, options, signer);
+
+  // Check if a contract with this name and chain is already deployed
+  const possiblePath = `${deploymentsPath}/${hre.network.name}/${name}.json`;
+  if (fs.existsSync(possiblePath)) {
+    return fetchFile(possiblePath);
+  }
+
+  const contract = await factory.deploy(...args, overrides);
+
+  return await saveContract(contract, args, artifact);
 }
 
 export async function createAlwayTier(
@@ -214,6 +318,9 @@ export async function createAlwayTier(
   }
   if (estimation === "aggressive") {
     estimationLevel = EstimationLevel.AGGRESSIVE;
+  }
+  if (estimation === "fixed") {
+    estimationLevel = EstimationLevel.FIXED;
   }
 
   const feeCalculated = await estimateGasFee(estimationLevel);
@@ -271,40 +378,70 @@ export async function createAlwayTier(
 }
 
 /**
- * Convert all the BigNumbers values that exist in an Object to string .
- * @param obj The object that contain BigNumbers
+ * Write a file
+ * @param _path Location of the file
+ * @param file The file
  */
-export const flattenBigNumbers = (obj: unknown): void => {
-  Object.keys(obj).forEach((key) => {
-    if (ethers.BigNumber.isBigNumber(obj[key])) {
-      obj[key] = obj[key].toString();
-    }
-  });
+export const writeFile = (
+  _path: string,
+  file: string | NodeJS.ArrayBufferView
+): void => {
+  try {
+    fs.writeFileSync(_path, file);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 /**
- * Estimate the gas that will be use to deploy the contract.
- * The contract provided could be a artifact or a contract
- * name that will be searched with Hardhat.
- * @param contract The contract that will be deployed.
- * @param args The arguments necessaries to deploy
- * @returns The gas estimation
+ * Read a file that contain a DeployResult
+ * @param _path Path of the DeployResult file
+ * @returns The DeployResult
  */
-export const estimateGasDeploy = async (
-  contract: string | Artifact | BasicArtifact,
-  args: any[] = []
-): Promise<BigNumber> => {
-  let artifact: Artifact | BasicArtifact;
-
-  if (typeof contract === "string") {
-    artifact = await artifacts.readArtifact(contract);
-  } else {
-    artifact = contract;
+export const fetchFile = (_path: string): DeployResult => {
+  try {
+    return JSON.parse(fs.readFileSync(_path).toString());
+  } catch (error) {
+    console.log(error);
   }
+};
 
-  const iface = new ethers.utils.Interface(artifact.abi);
-  const factory = new ethers.ContractFactory(iface, artifact.bytecode);
+/**
+ * Save the address that were deployed and created/copy to correct commit folder.
+ * Avoid to save/generate to localhost network
+ */
+export const save = async (): Promise<void> => {
+  await saveListAddresses();
+  copyDeployFolder();
+};
 
+/**
+ * Get the the artifact, factory and arguments of a contract depending
+ * of a specific deploy options/configuration.
+ * @remarks
+ * This is intended to be used when deploy-hardhat is not available;
+ *
+ * @param name The contract name
+ * @param options The deployment options
+ * @param signer The signer that will be connected to the contract factory
+ * @returns The artifact, factory and arguments of the specific contract
+ */
+async function getContract(
+  name: string,
+  options: DeployOptions,
+  signer: SignerWithAddress
+): Promise<{
+  artifact: BasicArtifact;
+  factory: ContractFactory;
+  args: any[];
+}> {
+  const artifact = options.contract
+    ? options.contract
+    : await artifacts.readArtifact(name);
+
+  const factory = ContractFactory.fromSolidity(artifact).connect(signer);
+
+  const args = options.args ? [...options.args] : [];
   const numArguments = factory.interface.deploy.inputs.length;
   if (args.length !== numArguments) {
     throw new Error(
@@ -312,9 +449,99 @@ export const estimateGasDeploy = async (
     );
   }
 
-  const data = factory.getDeployTransaction(...args).data;
+  return {
+    artifact,
+    factory,
+    args,
+  };
+}
 
-  return await ethers.provider.estimateGas({ data: data });
+/**
+ * Save the deployment contract information in local disk
+ * @param contract The contract instance that was deployed
+ * @param args The arguments used in the deployement
+ * @param artifact The artifact of the contract deployed
+ * @returns The deployment result with full information
+ */
+async function saveContract(
+  contract: Contract,
+  args: any[] = [],
+  artifact: BasicArtifact
+): Promise<DeployResult> {
+  const deployTx = contract.deployTransaction;
+  const receipt = await deployTx.wait();
+
+  flattenBigNumbers(deployTx);
+  flattenBigNumbers(receipt);
+
+  const result: DeployResult = {
+    address: contract.address,
+    abi: artifact.abi,
+    transactionHash: deployTx.hash,
+    receipt: receipt,
+    args: args,
+    numDeployments: 1,
+    bytecode: artifact.bytecode,
+    deployedBytecode: artifact.deployedBytecode,
+  };
+
+  await getExtendedInfo(result, artifact);
+
+  const path = `${deploymentsPath}/${hre.network.name}`;
+  if (!fs.existsSync(path)) {
+    fs.mkdirSync(path, { recursive: true });
+  }
+
+  // const pathChainId = `${deploymentsPath}/${await hre.getChainId()}`;
+  const pathChainId = `${deploymentsPath}/${hre.network.name}/.chainId`;
+  if (!fs.existsSync(pathChainId)) {
+    try {
+      writeFile(pathChainId, await hre.getChainId());
+    } catch (error) {
+      console.log(error);
+      console.log(
+        "ERROR: Cannot create the file with the chain. Please, create one manually (WIP)\n"
+      );
+    }
+  }
+
+  const pathFile = `${path}/${artifact.contractName}.json`;
+  writeFile(pathFile, JSON.stringify(result, null, 2));
+
+  return result;
+}
+
+/**
+ * Get the full extended info of a contract using the hardhat enviroment
+ * to save the major information posible about the deployment.
+ * @param result The deploy result of the contract
+ * @param artifact The artifact of the contrac that was the deployed
+ */
+async function getExtendedInfo(result: DeployResult, artifact: BasicArtifact) {
+  const contractId = `:${artifact.contractName}`;
+  const qualifiedName = (await artifacts.getAllFullyQualifiedNames()).find(
+    (e) => e.includes(contractId)
+  );
+  const buildInfo = await artifacts.getBuildInfo(qualifiedName);
+  const contractPath = qualifiedName.replace(contractId, "");
+  const hhOutput: OutputContract =
+    buildInfo.output.contracts[contractPath][artifact.contractName];
+
+  result.metadata = artifact.metadata ? artifact.metadata : hhOutput.metadata;
+  result.devdoc = artifact.devdoc ? artifact.devdoc : hhOutput.devdoc;
+  result.userdoc = artifact.userdoc ? artifact.userdoc : hhOutput.userdoc;
+}
+
+/**
+ * Convert all the BigNumbers values that exist in an Object to string .
+ * @param obj The object that contain BigNumbers
+ */
+const flattenBigNumbers = (obj: unknown): void => {
+  Object.keys(obj).forEach((key) => {
+    if (ethers.BigNumber.isBigNumber(obj[key])) {
+      obj[key] = obj[key].toString();
+    }
+  });
 };
 
 /**
@@ -327,10 +554,35 @@ export const estimateGasDeploy = async (
  * a best estimation, but also will take more time to calculate. Min is 1 and max is 20 for safety
  * @returns And object with all the information in wei. If the network support EIP-1559, gasPrice will be empty/undefined
  */
-export const estimateGasFee = async (
+const estimateGasFee = async (
   estimationLevel: EstimationLevel,
   txsTofetch = 10
 ): Promise<FeeData> => {
+  let multiplierLevel: number;
+
+  // If fixed, just return the fixed gasPrice selected
+  if (estimationLevel == EstimationLevel.FIXED) {
+    const gasPrice = ethers.utils.parseUnits(
+      deployConfig.gasPrice.toString(),
+      "gwei"
+    );
+    return {
+      maxFeePerGas: null,
+      maxPriorityFeePerGas: null,
+      gasPrice: ethers.BigNumber.from(gasPrice),
+    };
+  } else if (estimationLevel <= EstimationLevel.AGGRESSIVE) {
+    if (estimationLevel == EstimationLevel.LOW) {
+      multiplierLevel = 90;
+    } else if (estimationLevel == EstimationLevel.MARKET) {
+      multiplierLevel = 100;
+    } else {
+      multiplierLevel = 110;
+    }
+  } else {
+    multiplierLevel = estimationLevel;
+  }
+
   /*
    ** If `maxFeePerGas` exist in the response, then the network have EIP-1559 Transaction Support
    **  - NetworkType = 0 - Have not EIP-1559 Transaction Support
@@ -427,19 +679,6 @@ export const estimateGasFee = async (
     }
   );
 
-  let multiplierLevel: number;
-  if (estimationLevel <= EstimationLevel.AGGRESSIVE) {
-    if (estimationLevel == EstimationLevel.LOW) {
-      multiplierLevel = 90;
-    } else if (estimationLevel == EstimationLevel.MARKET) {
-      multiplierLevel = 100;
-    } else {
-      multiplierLevel = 110;
-    }
-  } else {
-    multiplierLevel = estimationLevel;
-  }
-
   // TODO: Maybe add a field, so someone can set a MAX value on that attribute that the user is willing to spend ???
   Object.keys(result).forEach((key) => {
     if (ethers.BigNumber.isBigNumber(result[key])) {
@@ -453,44 +692,6 @@ export const estimateGasFee = async (
   });
 
   return result;
-};
-
-/**
- * Write a file
- * @param _path Location of the file
- * @param file The file
- */
-export const writeFile = (
-  _path: string,
-  file: string | NodeJS.ArrayBufferView
-): void => {
-  try {
-    fs.writeFileSync(_path, file);
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-/**
- * Read a file that contain a DeployResult
- * @param _path Path of the DeployResult file
- * @returns The DeployResult
- */
-export const fetchFile = (_path: string): DeployResult => {
-  try {
-    return JSON.parse(fs.readFileSync(_path).toString());
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-/**
- * Save the address that were deployed and created/copy to correct commit folder.
- * Avoid to save/generate to localhost network
- */
-export const save = async (): Promise<void> => {
-  await saveListAddresses();
-  copyDeployFolder();
 };
 
 const copyDeployFolder = (): void => {
@@ -559,7 +760,7 @@ const assignValue = (obj: any, keyPath: string[], value: any) => {
  * @param code - the opcode
  * @param erand - the operand, currently limited to 1 byte (defaults to 0)
  */
-export function op(
+function op(
   code: number,
   erand: number | BytesLike | utils.Hexable = 0
 ): Uint8Array {
@@ -573,7 +774,7 @@ export function op(
  * @param bytesLength - (defaults to 1) number of bytes to left pad if `value` doesn't completely fill the desired amount of memory. Will throw `InvalidArgument` error if value already exceeds bytes length.
  * @returns {Uint8Array} - raw bytes representation
  */
-export function bytify(
+function bytify(
   value: number | BytesLike | utils.Hexable,
   bytesLength = 1
 ): BytesLike {
